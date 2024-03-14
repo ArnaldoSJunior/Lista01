b607dca3358d10e4f1a05a1e5e7c1ee0ab8b65b8
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-14 15:53:46 -0300

    Exercicio04

--- program.java
+++ program.java
@@ -3,7 +3,8 @@ public class program {
     public static void main(String[] args){
        // Exercicio01.executar();
        //Exercicio02.executar();
-        Exercicio03.executar();
+       //Exercicio03.executar();
+        Exercicio04.executar();
     }
     }
 
