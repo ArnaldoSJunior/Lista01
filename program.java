c1c71449c859d5c5e514bda201898176f6ce6862
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-17 11:42:48 -0300

    Exercicio10

--- program.java
+++ program.java
@@ -10,6 +10,18 @@ public class program {
         //Exercicio06.executar();
         //Exercicio07.executar();
         //Exercicio08.executar();
-        Exercicio09.executar();
+        //Exercicio09.executar();
+        Exercicio10.executar();
+        //Exercicio11.executar();
+        //Exercicio12.executar();
+        //Exercicio13.executar();
+        //Exercicio14.executar();
+        //Exercicio15.executar();
+        //Exercicio16.executar();
+        //Exercicio17.executar();
+        //Exercicio18.executar();
+        //Exercicio19.executar();
+        //Exercicio20.executar();
+
     }
 }
